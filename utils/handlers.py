53f58ed256cb54e375860edeccea026b0eac03d2
IkramKarimov <ikramkarimov@gmail.com>
2025-04-22 17:52:45 +0500

    Add files via upload

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,18 +1,10 @@
-from aiogram import types, Dispatcher
-from aiogram.types import InputMediaPhoto
-from config import CHANNEL_ID
-from utils.watermark import add_watermark
-import os
+from aiogram import Router, types
 
-user_sessions = {}
+router = Router()
 
-async def start_command(message: types.Message):
-    await message.answer("Привет! Напиши /add чтобы добавить объект недвижимости.")
+@router.message(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Добро пожаловать в AlmaDomBot!")
 
-async def add_command(message: types.Message):
-    user_sessions[message.from_user.id] = {'step': 'type'}
-    await message.answer("Выберите тип недвижимости: Квартира, Дом, Таунхаус, Коммерческая недвижимость, Участок")
-
-def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(start_command, commands="start")
-    dp.register_message_handler(add_command, commands="add")
+def register_handlers(dp):
+    dp.include_router(router)
