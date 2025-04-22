701feaf7f1a869f7b6352f021c95f0b97e8a734f
IkramKarimov <ikramkarimov@gmail.com>
2025-04-22 12:56:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from flask import Flask, request
 import telegram
+import os
 
-TOKEN = '7513357606:AAEiNTAuIVn3CL_-txzYIl2Pa7VSVzHRFwQ'
+TOKEN = os.environ.get('BOT_TOKEN')
 bot = telegram.Bot(token=TOKEN)
+
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'AlmaDomBot is running'
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-    if update.message and update.message.text == '/start':
+    if update.message:
         chat_id = update.message.chat.id
-        bot.send_message(chat_id=chat_id, text='Привет! Я АлмаДомБот — бот для подачи и фильтрации объектов')
+        text = update.message.text
+
+        if text == '/start':
+            bot.send_message(chat_id=chat_id, text='Привет! Я АлмаДомБот — бот для подачи и фильтрации объектов')
     return 'ok'
 
-@app.route('/')
-def home():
-    return 'Бот работает!'
-
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
