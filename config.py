b6f068344ba8cfd4c6e8b09bd10136dfc635fd10
IkramKarimov <ikramkarimov@gmail.com>
2025-04-22 18:09:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-APP_URL = os.getenv("APP_URL", "https://your-render-url.onrender.com")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7513357606:AAEiNTAuIVn3CL_-txzYIl2Pa7VSVzHRFwQ")
+APP_URL = os.getenv("APP_URL", "https://almadom-bot.onrender.com")
