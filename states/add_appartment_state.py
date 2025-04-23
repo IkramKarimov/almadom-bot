cd45469fa6e599b753d633789547cc58dc2a7cf8
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 15:54:49 +0600

    Update add_appartment_state.py

--- states/add_appartment_state.py
+++ states/add_appartment_state.py
@@ -1,1 +1,12 @@
-test
+from aiogram.fsm.state import State, StatesGroup
+
+class AddApartment(StatesGroup):
+    district = State()
+    rooms = State()
+    complex_name = State()
+    year_built = State()
+    price = State()
+    area = State()
+    floor_info = State()
+    media = State()
+    confirm = State()
