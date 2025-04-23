7688e6db3ef468af51ae109a965d35132c5a7c49
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 16:16:13 +0600

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -7,3 +7,12 @@ done_upload_kb = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True
 )
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+confirm_post_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Опубликовать", callback_data="confirm_post")],
+        [InlineKeyboardButton(text="Отменить", callback_data="cancel_post")]
+    ]
+)
