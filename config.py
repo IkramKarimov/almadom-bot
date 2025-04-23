47f38ea2847d8cd7ecdd27c57b9302b7d014f837
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 12:36:12 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # или просто строкой: 'your_token_here'
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # или: 'https://your-app-name.onrender.com'
 WEBHOOK_PATH = f"/{TOKEN}"
 WEB_SERVER_HOST = "0.0.0.0"
 WEB_SERVER_PORT = int(os.getenv("PORT", default=8000))
