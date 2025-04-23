5ace272f6965a479e45888759dbe7f2ad87ee108
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 17:15:39 +0600

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -10,10 +10,6 @@ router = Router()
 async def cmd_start(message: Message):
     await message.answer("Добро пожаловать в АлмаДомБот!")
 
-@router.message(Command("add"))
-async def cmd_add(message: Message):
-    await message.answer("Форма добавления объекта в разработке. Ожидайте!")
-
 def register_handlers(dp):
     dp.include_router(router)
 
