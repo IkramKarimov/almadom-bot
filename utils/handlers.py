2a2534788869c80cfea76106e47e99d874865535
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 18:35:16 +0600

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -21,5 +21,5 @@ def register_handlers(dp):
 
 @router.message(F.text == "Квартира")
 async def start_flat_creation(message: Message, state: FSMContext):
-    await state.set_state(FlatForm.district)
+    await state.set_state(AddApartment.district)
     await message.answer("Выберите район:", reply_markup=get_district_keyboard())
