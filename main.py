0b1382af73e854a2d7b8c516ed01f727e17efbde
IkramKarimov <ikramkarimov@gmail.com>
2025-04-23 12:33:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
 import asyncio
+import logging
+from typing import Any
+
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
@@ -9,22 +11,47 @@ from aiohttp import web
 from handlers import router
 from config import TOKEN, WEBHOOK_URL
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 dp.include_router(router)
 
-async def on_startup(app):
-    await bot.set_webhook(f"{WEBHOOK_URL}/{TOKEN}")
+async def on_startup(app: web.Application) -> None:
+    """Функция запуска приложения."""
+    try:
+        await bot.set_webhook(f"{WEBHOOK_URL}/{TOKEN}")
+        logger.info("Webhook успешно установлен.")
+    except Exception as e:
+        logger.error(f"Ошибка при установке webhook: {e}")
 
-async def on_shutdown(app):
-    await bot.delete_webhook()
+async def on_shutdown(app: web.Application) -> None:
+    """Функция остановки приложения."""
+    try:
+        await bot.delete_webhook()
+        logger.info("Webhook успешно удален.")
+    except Exception as e:
+        logger.error(f"Ошибка при удалении webhook: {e}")
 
-app = web.Application()
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
-app.router.add_routes([
-    web.post(f"/{TOKEN}", setup_application(app=app, dispatcher=dp, bot=bot)),
-])
+def create_app() -> web.Application:
+    """Создание и настройка веб-приложения."""
+    app = web.Application()
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    app.router.add_routes([
+        web.post(f"/{TOKEN}", setup_application(app=app, dispatcher=dp, bot=bot)),
+    ])
+
+    return app
 
 if __name__ == "__main__":
-    web.run_app(app, port=8000)
+    try:
+        logger.info("Запуск веб-приложения...")
+        app = create_app()
+        web.run_app(app, port=8000)
+    except Exception as e:
+        logger.error(f"Ошибка при запуске приложения: {e}")
