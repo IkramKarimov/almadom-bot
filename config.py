afe691682ce239dc41d06a4aba7b1dcbe0d6ae95
IkramKarimov <ikramkarimov@gmail.com>
2025-04-24 21:36:34 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 TOKEN = os.getenv("TOKEN", "7513357606:AAEiNTAuIVn3CL_-txzYIl2Pa7VSVzHRFwQ")  # или просто строкой: 'your_token_here'
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://almadom-bot.onrender.com","https://almadom-bot-production-3cb4.up.railway.app")
+WEBHOOK_URL = os.getenv("https://almadom-bot.onrender.com","https://almadom-bot-production-3cb4.up.railway.app")
 WEBHOOK_PATH = f"/{TOKEN}"
 WEB_SERVER_HOST = "0.0.0.0"
 WEB_SERVER_PORT = int(os.getenv("PORT", default=8000))
