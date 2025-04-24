432e944f2f8a8b33f7bdb3129e9f3276d79a7aa3
IkramKarimov <ikramkarimov@gmail.com>
2025-04-24 12:44:47 +0600

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -3,8 +3,12 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, F
-from utils.keyboards import property_type_kb, get_district_keyboard
-from utils.keyboards import property_type_kb, get_room_count_keyboard
+from utils.keyboards import (property_type_kb, 
+    get_district_keyboard,
+    get_room_count_keyboard,
+    get_preview_keyboatd,
+    get_contact_keyboard,
+)
 from states.add_appartment_state import AddApartment
 
 router = Router()
