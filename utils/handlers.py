e1e6dc8f5186feb5d37f9b52778250e823e3e130
IkramKarimov <ikramkarimov@gmail.com>
2025-04-24 19:00:16 +0600

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -48,7 +48,7 @@ async def ask_address(message: Message, state: FSMContext):
     
 @router.message(AddApartment.address)
 async def ask_year_built(message: Message, state: FSMContext):
-    await state.update_data(address=None if text == "-" else text)
+    await state.update_data(address=message.text.strip())
     await state.set_state(AddApartment.year_built)
     await message.answer("Укажите год постройки:")
     
