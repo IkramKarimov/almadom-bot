01b0854fc1160fd6cc156f6cd0a83de0535de037
IkramKarimov <ikramkarimov@gmail.com>
2025-04-24 21:13:35 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 import os
 
-TOKEN = os.getenv("TOKEN")  # или просто строкой: 'your_token_here'
+TOKEN = os.getenv("TOKEN", "7513357606:AAEiNTAuIVn3CL_-txzYIl2Pa7VSVzHRFwQ")  # или просто строкой: 'your_token_here'
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 WEBHOOK_PATH = f"/{TOKEN}"
 WEB_SERVER_HOST = "0.0.0.0"
