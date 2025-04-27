67f48729bd4689fcb3c0d42a93107280c7b09669
IkramKarimov <ikramkarimov@gmail.com>
2025-04-27 15:00:01 +0500

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -118,7 +118,7 @@ def add_more_media_keyboard():
 def get_contact_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="Отправить контакт", request_contact=True)]
+            [KeyboardButton(text="☎️ Отправить контакт", request_contact=True)]
         ],
         resize_keyboard=True,
         one_time_keyboard=True
