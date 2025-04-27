3664b19a2c3d5b3fa87f5f23c050d1d46449ef4e
IkramKarimov <ikramkarimov@gmail.com>
2025-04-27 13:43:56 +0500

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -37,7 +37,7 @@ async def cmd_start(message: Message):
     await message.answer("Добро пожаловать в АлмаДомБот!")
     
 @router.callback_query(lambda c: c.data == "start")
-async def start_adding(callback: CallbackQuery, state: FSMContext):
+async def start(callback: CallbackQuery, state: FSMContext):
     await state.clear()
     await callback.message.answer("Давайте добавим новый объект!", reply_markup=property_type_kb())
     
